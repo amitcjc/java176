38d9de6ed6494a58d45d0badf8537130fd376e68
amitcjc <amitcjcpune@gmail.com>
2024-06-21 17:02:07 +0530

    by xyz

--- GitDemoB176/src/com/Demo.java
+++ GitDemoB176/src/com/Demo.java
@@ -2,6 +2,13 @@ package com;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("This is M1");
+	}
+	
+	
+	
 public static void main(String[] args)
 {
 System.out.println("This is main Mathod");	
